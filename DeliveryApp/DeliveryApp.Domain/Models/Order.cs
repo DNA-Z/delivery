5be56c43e271759b1ee78e30e9f68b5f7f43b357
Inna Borodina <creator.geek@gmail.com>
2025-07-14 15:37:55 +0300

    Location added

---
+++ DeliveryApp/DeliveryApp.Domain/Models/Order.cs
@@ -0,0 +1,35 @@
+﻿using CSharpFunctionalExtensions;
+using DeliveryApp.Domain.Models.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryApp.Domain.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Location Location { get;}
+        public int Volume { get;}
+        public OrderStatus? Status { get;}
+        public Guid CourierId { get; }
+
+        private Order() { }
+
+        private Order(Guid orderId, Location location, int volume)
+        {
+            CourierId = orderId;
+            Location = location;
+            Volume = volume;
+        }
+
+        public static Result<Order> Create (Guid orderId, Location location, int volume)
+        {
+            if () return ResultWithError.Fail("some error info");
+        }
+
+        public UnitResult<Error>
+    }
+}
---
+++ DeliveryApp/DeliveryApp.Domain/Models/ValueObjects/Location.cs
@@ -0,0 +1,57 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryApp.Domain.Models.ValueObjects
+{
+    public class Location : ValueObject
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        private Location() { }
+
+        public Location(int x, int y)
+        {
+            SetX(x);
+            SetY(y);
+        }
+
+        public static Result<Location> Create(int x, int y)
+        {
+            var result = new Location(x, y);
+            return result;
+        }
+
+        public static Result<Location> CreateRandom()
+        {
+            var random = new Random();
+            return new Location(random.Next(1, 100), random.Next(1, 100));
+        }
+
+        private void SetX(int x) 
+        {
+            if (x <= 0)
+                throw new ArgumentOutOfRangeException(nameof(x));
+
+            X = x;
+        }
+
+        private void SetY(int y)
+        {
+            if (y <= 0)
+                throw new ArgumentOutOfRangeException(nameof(y));
+
+            Y = y;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return X;
+            yield return Y;
+        }
+    }
+}
