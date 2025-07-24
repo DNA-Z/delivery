312af96eefed5c4ea92084227fda9297b496f3f1
Inna Borodina <creator.geek@gmail.com>
2025-07-24 09:58:07 +0300

    order status added

---
+++ DeliveryApp/DeliveryApp.Domain/Models/OrderAggregate/OrderStatus.cs
@@ -0,0 +1,27 @@
+﻿using CSharpFunctionalExtensions;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DeliveryApp.Domain.Models.OrderAggregate
+{
+    public class OrderStatus : ValueObject
+    {
+        public static OrderStatus Created => new(nameof(Created).ToLowerInvariant());
+        public static OrderStatus Assigned => new(nameof(Assigned).ToLowerInvariant());
+        public static OrderStatus Completed => new(nameof(Completed).ToLowerInvariant());
+
+        public string Name { get; private set; } = string.Empty;
+
+        [ExcludeFromCodeCoverage]
+        private OrderStatus() { }
+
+        private OrderStatus(string name) : this()
+        {
+            Name = name;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Name;
+        }
+    }
+}
---
+++ DeliveryApp/DeliveryApp.Domain/Models/SharedKernel/Location.cs
@@ -0,0 +1,80 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace DeliveryApp.Domain.Models.SharedKernel
+{
+    public class Location : ValueObject
+    {
+        public int X { get; private set; } = 1;
+        public int Y { get; private set; } = 1;
+
+        private Location() { }
+
+        public Location(int x, int y)
+        {
+            SetX(x);
+            SetY(y);
+        }
+
+        public static Result<Location, Error> Create(int x, int y)
+        {
+            var result = new Location(x, y);
+            return result;
+        }
+
+        public Result<int, Error> DistanceTo(Location target)
+        {
+            var distance = Math.Abs(target.X - X);
+            distance += Math.Abs(target.Y - Y);
+
+            return distance;
+        }
+
+        public static Result<Location> CreateRandom()
+        {
+            var random = new Random();
+            return new Location(random.Next(1, 11), random.Next(1, 11));
+        }
+
+        private UnitResult<Error> SetX(int x) 
+        {
+            if (x == 0) return Errors.LocationCannotBeEqualToZero(x);
+            if (x < 0) return Errors.LocationCannotBeLessThanZero(x);
+            if (x > 10) return Errors.LocationCannotBeMoreThanTen(x);
+
+            X = x;
+        }
+
+        private UnitResult<Error> SetY(int y)
+        {
+            if (y == 0) return Errors.LocationCannotBeEqualToZero(y);
+            if (y < 0) return Errors.LocationCannotBeLessThanZero(y);
+            if (y > 10) return Errors.LocationCannotBeMoreThanTen(y);
+
+            Y = y;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return X;
+            yield return Y;
+        }
+
+        public static class Errors
+        {
+            public static Error LocationCannotBeEqualToZero(int position)
+            {
+                return new Error($"{nameof(position)} can't be equal to 0");
+            }
+
+            public static Error LocationCannotBeLessThanZero(int position)
+            {
+                return new Error($"{nameof(position)} can't be less than 0");
+            }
+
+            public static Error LocationCannotBeMoreThanTen(int position)
+            {
+                return new Error($"{nameof(position)} can't be more than 10");
+            }
+        }
+    }
+}
