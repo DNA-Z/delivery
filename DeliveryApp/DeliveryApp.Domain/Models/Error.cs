4f55025f5407b23e88f6a3dc20ed4cf77f46c7d0
Inna Borodina <creator.geek@gmail.com>
2025-07-24 10:31:01 +0300

    error construct error

--- DeliveryApp/DeliveryApp.Domain/Models/Error.cs
+++ DeliveryApp/DeliveryApp.Domain/Models/Error.cs
@@ -4,7 +4,13 @@ namespace DeliveryApp.Domain.Models
     public readonly struct Error
     {
         public string Message { get; }
+        public string? Description { get; }
         public Error(string message) => Message = message;
-        public override string ToString() => Message;
+        public Error(string message, string description)
+        {
+            Message = message;
+            Description = description;
+        }
+        public override string ToString() => $"{Message}: {Description ?? string.Empty}";
     }
 }
